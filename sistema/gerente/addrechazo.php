34e2dd86c0ca31fd27314835ef5e184bab6e28cc
cvmichus <c.michaus@incapital.mx>
2019-09-30 00:35:32 -0500

    ADD
    ADD

--- sistema/gerente/addrechazo.php
+++ sistema/gerente/addrechazo.php
@@ -19,16 +19,68 @@ session_start();
 
         /*para mamndar el correo al usuario*/
         $sqlUsuario = "SELECT * FROM `tbl_usuarios` as u INNER JOIN `tbl_empleados` as e ON u.CodUsuario = e.CodUsu WHERE u.estatus = 1 AND u.CodUsuario = ".$_POST["CodEmpleado"]." ";
-        if($resqryUsuario = $mysqli->query($sqlUsuario)) {
+        $resqryUsuario = $mysqli->query($sqlUsuario);
         $data = mysqli_fetch_assoc($resqryUsuario);
         $CorreoEmpleado = $data['Correo'];
-        }
+        
+
+         if($EstatusSolicitud == 1 ){//ACEPTADO
 
 
-                                        if($EstatusSolicitud == 0 ){//rechazo
+                      $consulta1 = "INSERT INTO `tbl_rasolicitud` (`CodRAS`, `CodSol`,  `CodUsuario`,`Motivo`, `FechaAR`, `HoraAR`, `EstatusSolicitud`, `Tipo`) VALUES (NULL,'".$_POST["CodS"]."', '".$_POST["CodEmpleado"]."', '".$_POST['observaciones']."', '".$fecha_del_dia."', '".$hora_actual."','".$_POST['EstatusSol']."','2')";             
+                      if($resultado1 = $mysqli->query($consulta1)) {
 
-                            
-             require("../PHPMailer-master/src/PHPMailer.php");
+                      $consulta2 = "UPDATE `tbl_solicitud` SET `Estatus` = '".$_POST['EstatusSol']."' WHERE  CodSol = '".$_POST["CodS"]."' ";             
+                      if($resultado2 = $mysqli->query($consulta2)) {
+
+
+                          $sqltrab = "SELECT * FROM  `tbl_empleados`   WHERE  CodUsu  = ".$_POST["CodEmpleado"]." ";
+                        if($qrytrab = $mysqli->query($sqltrab)) {
+                        $datatrab = mysqli_fetch_assoc($qrytrab);
+                        $DiasVacPHP = $datatrab['DiasVac'];
+
+
+                          $sqlsol = "SELECT * FROM  `tbl_solicitud`  WHERE  CodUsuario  = ".$_POST["CodEmpleado"]." ";
+                              if($qrysol = $mysqli->query($sqlsol)) {
+                              $datasol = mysqli_fetch_assoc($qrysol);
+                              $DiasSolPHP = $datasol['DiasSolicitados'];
+
+                                 $total = $DiasVacPHP - $DiasSolPHP;
+
+
+                         $consulta3 = "UPDATE `tbl_empleados`  SET `DiasVac` = ".$total." WHERE  CodUsu = ".$_POST["CodEmpleado"]." ";  
+                                    if($resultado3 = $mysqli->query($consulta3)) {
+                                          header("Location: index.php");
+                                    }else{
+                                         ECHO "ERROR";
+                                         echo mysql_errno($consulta3);
+                                    }
+                      
+                              //
+
+                              }
+
+
+
+                      }
+
+                    
+                     
+
+                      }
+
+                      }
+
+         }
+         else if($EstatusSolicitud == 0){  /*ceptado*/
+
+                 $consulta1 = "INSERT INTO `tbl_rasolicitud` (`CodRAS`, `CodSol`,  `CodUsuario`,`Motivo`, `FechaAR`, `HoraAR`, `EstatusSolicitud`, `Tipo`) VALUES (NULL,'".$_POST["CodS"]."', '".$_POST["CodEmpleado"]."', '".$_POST['observaciones']."', '".$fecha_del_dia."', '".$hora_actual."','".$_POST['EstatusSol']."','1')";             
+                      if($resultado1 = $mysqli->query($consulta1)) {
+
+                      $consulta2 = "UPDATE `tbl_solicitud` SET `Estatus` = '".$_POST['EstatusSol']."' WHERE  CodSol = '".$_POST["CodS"]."' ";             
+                      if($resultado2 = $mysqli->query($consulta2)) {
+
+                         require("../PHPMailer-master/src/PHPMailer.php");
                           require("../PHPMailer-master/src/SMTP.php");
                           require("../PHPMailer-master/src/Exception.php");
 
@@ -81,117 +133,18 @@ session_start();
                           // echo "Mailer Error: " . $mail->ErrorInfo;
                           echo "Error al enviar Mensaje";
                           } else {
-                          $alerta = "guardado";
+                          //$alerta = "guardado";
+                           header("Location: index.php");  
                           }
 
-
-    $consulta1 = "INSERT INTO `tbl_rasolicitud` (`CodRAS`, `CodSol`,  `CodUsuario`,`Motivo`, `FechaAR`, `HoraAR`, `EstatusSolicitud`, `Tipo`) VALUES (NULL,'".$_POST["CodS"]."', '".$_POST["CodEmpleado"]."', '".$_POST['observaciones']."', '".$fecha_del_dia."', '".$hora_actual."','".$_POST['EstatusSol']."','1')";             
-    if($resultado1 = $mysqli->query($consulta1)) {
-    
-    $consulta2 = "UPDATE `tbl_solicitud` SET `Estatus` = '".$_POST['EstatusSol']."' WHERE  CodSol = '".$_POST["CodS"]."' ";             
-  if($resultado2 = $mysqli->query($consulta2)) {
-
-         header("Location: index.php");  
-
-  }
-
-}
-
-                                        }
-
-            else if($EstatusSolicitud == 1){  /*ceptado*/
-
-                     $CorreoEmpleado2 = $data['Correo'];
-
-                          require("../PHPMailer-master/src/PHPMailer.php");
-                          require("../PHPMailer-master/src/SMTP.php");
-                          require("../PHPMailer-master/src/Exception.php");
-
-
-                          $mail = new PHPMailer\PHPMailer\PHPMailer();
-                          $mail->IsSMTP(); 
-
-                          $mail->CharSet="UTF-8";
-                           $mail->Host = "smtp.office365.com";
-                          //$mail->SMTPDebug = 2; 
-                          $mail->Port = 587; //465 or 587
-
-                          $mail->SMTPSecure = 'tls';  
-                          $mail->SMTPAuth = true; 
-                          $mail->IsHTML(true);
-
-                          //Authentication
-                           $mail->Username = "recursos.humanos@wri.org";
-                          $mail->Password = "WRIm3x1c086!";
-
-                          //Set Params
-                          $mail->SetFrom("recursos.humanos@wri.org");
-                          $mail->AddAddress($CorreoEmpleado2);
-                          $mail->AddAddress("michusvalentin@hotmail.com");
-                     
-
-                          $mail->Subject = "Solicitud de Vacaciones Aprobada";
-                          $mail->Body = '
-                          <html>
-                          <head>
-                          <title>Bienvenido</title>
-                          </head>
-                          <body>
-                          <h1>
-                          Notificacion de Solicitud de Vacaciones Apropbada:
-                          </h1>
-                          <p>
-                          
-              Hola estimado Usuario  tu solicitud ha sido aprobada <br>
-              Para revisar tus dias restantes segir el siguiente link:
-                          <br>
-                          http:localhost/sistemadevacaciones/index.php
-                          </p>
-                          </body>
-                          </html>
-                          ';
-
-
-                          if(!$mail->Send()) {
-                          // echo "Mailer Error: " . $mail->ErrorInfo;
-                          echo "Error al enviar Mensaje";
-                          } else {
-                                $alerta = "guardado";
-                          }
-
-
-                $consulta1 = "INSERT INTO `tbl_rasolicitud` (`CodRAS`, `CodSol`, `CodUsuario`,`Motivo`, `FechaAR`, `HoraAR`, `EstatusSolicitud`,`Tipo`) 
-                VALUES (NULL,'".$_POST["CodS"]."', '".$CodEmpleado ."', '".$_POST['observaciones']."', '".$fecha_del_dia."', '".$hora_actual."','".$_POST['EstatusSol']."','2')";    
-
-                    if($resultado1 = $mysqli->query($consulta1)) {/* SE GUARDO LA CONSULTA */
-
-                         
-                        $sqltrab = "SELECT * FROM  `tbl_empleados`   WHERE  CodUsu  = ".$_POST["CodEmpleado"]." ";
-                        if($qrytrab = $mysqli->query($sqltrab)) {
-                        $datatrab = mysqli_fetch_assoc($qrytrab);
-                        $DiasVacPHP = $datatrab['DiasVac'];
-
-
-                              $sqlsol = "SELECT * FROM  `tbl_solicitud`  WHERE  CodUsuario  = ".$_POST["CodEmpleado"]." ";
-                              if($qrysol = $mysqli->query($sqlsol)) {
-                              $datasol = mysqli_fetch_assoc($qrysol);
-                              $DiasSolPHP = $datasol['DiasVac'];
-
-                               echo $total = $DiasVacPHP - $DiasSolPHP;
-
-
-                              }
-
-                         
-
-                        }
+                      }
 
                     }
 
+         }
 
-                  
-            }/* FIN DEL IF*/
-        /**/
+
+                                       
 }else {
     header("Location: ../index.php");
   }
--- sistema/gerente/logout.php
+++ sistema/gerente/logout.php
@@ -6,17 +6,15 @@ session_start();
       require("../../class/cnmysql.php");
       date_default_timezone_set('America/Mexico_City');
 
-		$iduser = $_SESSION['CodUsuario'];
-		$fecha_actual= date('Y-m-d');//fecha actual
+    $iduser = $_SESSION['CodUsuario'];
+    $fecha_actual= date('Y-m-d');//fecha actual
 
-        $consulta = "UPDATE `tbl_usuarios` u SET u.en_session='0',ult_act_usu='".$fecha_actual."' WHERE u.cod_usuario = '".$iduser."' ";
-        if($resultado = $mysqli->query($consulta)) {
+        
+          @session_start();
+        session_destroy();
+        header("Location: ../../index.php");
 
-			    @session_start();
-				session_destroy();
-				header("Location: ../../index.php");
-
-        }
+        
 
 }
 
